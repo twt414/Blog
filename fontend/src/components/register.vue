12a6fee474c31c40ea2b99ddb2951b7ee51fd4eb
lius <liuswin@163.com>
2018-02-26 19:43:01 +0800

    register style supplement

--- fontend/src/components/register.vue
+++ fontend/src/components/register.vue
@@ -5,7 +5,7 @@
       <p class="control has-icons-left has-icons-right">
         <input class="input" type="email" placeholder="邮箱">
         <span class="icon is-small is-left">
-          <i class="fas fa-envelope"></i>
+          <svg class="svg-inline--fa fa-envelope fa-w-16" aria-hidden="true" data-fa-processed="" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg><!-- <i class="fas fa-envelope"></i> -->
         </span>
         <span class="icon is-small is-right">
           <i class="fas fa-check"></i>
@@ -16,7 +16,7 @@
       <p class="control has-icons-left">
         <input class="input" type="password" placeholder="设置密码">
         <span class="icon is-small is-left">
-          <i class="fas fa-lock"></i>
+          <svg class="svg-inline--fa fa-lock fa-w-16" aria-hidden="true" data-fa-processed="" data-prefix="fas" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path></svg>
         </span>
       </p>
     </div>
@@ -24,7 +24,7 @@
       <p class="control has-icons-left">
         <input class="input" type="password" placeholder="确认密码">
         <span class="icon is-small is-left">
-          <i class="fas fa-lock"></i>
+          <svg class="svg-inline--fa fa-lock fa-w-16" aria-hidden="true" data-fa-processed="" data-prefix="fas" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path></svg>
         </span>
       </p>
     </div>
@@ -41,24 +41,28 @@ export default {
   name: 'Register',
   data: function () {
     return {
-      a: 'a'
+      counter: 0
     }
   },
-  method: {
+  computed: {
+
+  },
+  methods: {
     register: function (ev) {
-      console.log('rem')
-      return {
-        a: 'a'
-      }
+
     }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .reg-container {
     width: 18rem;
+  }
+  .reg-title {
+    margin: 50px 0 20px 0;
+    font-size: 1.2rem;
   }
   /* .r-form-group {
     width: 18rem;
@@ -68,5 +72,8 @@ export default {
   }
   .register-btn {
     width: 100%;
+  }
+  .svg-inline--fa {
+    width: 0.9em;
   }
 </style>
